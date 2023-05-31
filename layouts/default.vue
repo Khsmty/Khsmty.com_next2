6bf7032f01fd24dbe792724a45724ea4e496a461
Khsmty <me@khsmty.com>
2023-05-31 23:43:45 +0900

    basic style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,21 @@
 <template>
-  layout
-  <slot />
+  <v-app>
+    <!-- navbar -->
+    <v-app-bar scroll-behavior="hide">
+      <v-app-bar-nav-icon @click="state.drawer = !state.drawer" />
+      <v-toolbar-title>Khsmty</v-toolbar-title>
+      <!-- <v-spacer />
+      <v-btn>Logout</v-btn> -->
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <slot />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+
+<script setup lang="ts">
+import state from "~/scripts/state";
+</script>
---
+++ scripts/state.ts
@@ -0,0 +1,5 @@
+import { reactive } from "vue";
+
+export default reactive({
+  drawer: false,
+});
