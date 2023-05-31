c3c8f595314268ad81b08b385e48a5780dc54612
Khsmty <me@khsmty.com>
2023-06-01 00:10:11 +0900

    temp

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,36 @@
 <template>
   <v-app>
+    <!-- drawer -->
+    <v-navigation-drawer v-model="state.drawer" app>
+      <v-list>
+        <v-list-item-group>
+          <v-list-item
+            v-for="item in drawerItems"
+            :key="item.title"
+            :to="item.to"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
     <!-- navbar -->
     <v-app-bar>
       <v-app-bar-nav-icon @click="state.drawer = !state.drawer" />
-      <v-toolbar-title>Khsmty</v-toolbar-title>
+
+      <v-toolbar-title class="toolbar-title" @click="$router.push('/')">
+        <v-avatar class="mr-2">
+          <v-img src="/img/icon.webp" />
+        </v-avatar>
+        Khsmty
+      </v-toolbar-title>
       <!-- <v-spacer />
       <v-btn>Logout</v-btn> -->
     </v-app-bar>
@@ -18,4 +45,19 @@
 
 <script setup lang="ts">
 import state from "~/scripts/state";
+
+const drawerItems = [
+  { title: "Home", icon: "mdi-home", to: "/" },
+  { title: "About", icon: "mdi-information", to: "/about" },
+  { title: "Contact", icon: "mdi-email", to: "/contact" },
+];
 </script>
+
+<style lang="scss">
+.toolbar-title > div {
+  cursor: pointer;
+  align-items: center;
+  display: flex;
+  font-size: 1.5rem;
+}
+</style>
