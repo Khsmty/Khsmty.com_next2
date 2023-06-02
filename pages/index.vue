7ee31f5584b1a76c55f7d3198e0b73f5a51a5e4f
Khsmty <me@khsmty.com>
2023-06-02 20:24:04 +0900

    top page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <p>Hello, World!</p>
+  <v-parallax src="/img/background.webp" height="400" class="top-image">
+    <v-row>
+      <v-col cols="12" class="profile">
+        <v-avatar size="130">
+          <v-img src="/img/icon.webp" />
+        </v-avatar>
+        <span class="ml-5">
+          <h1 style="font-size: 3rem">Khsmty</h1>
+          <p style="font-size: 1.3rem">くそざこプログラマー</p>
+        </span>
+      </v-col>
+    </v-row>
+  </v-parallax>
 </template>
 
 <script setup lang="ts">
@@ -7,3 +19,23 @@ useSeoMeta({
   description: "Khsmtyのウェブサイト",
 });
 </script>
+
+<style>
+.top-image {
+  border-radius: 10px;
+}
+.top-image > img {
+  filter: blur(5px);
+}
+.top-image > .v-responsive__content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.profile {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+}
+</style>
