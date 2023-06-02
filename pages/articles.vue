19d0a6d452a8f7d7cc818d70b9c397b3b120047e
Khsmty <me@khsmty.com>
2023-06-02 13:08:23 +0900

    fix style

--- pages/articles.vue
+++ pages/articles.vue
@@ -1,13 +1,26 @@
 <template>
-  <h1>記事一覧</h1>
-  <p class="mb-5">Khsmty が書いた記事の一覧です。</p>
+  <v-row justify="center">
+    <v-col cols="12" class="mt-3" style="max-width: 900px">
+      <h1>記事一覧</h1>
+      <p>Khsmty が書いた記事の一覧です。</p>
+    </v-col>
 
-  <v-row>
-    <v-col v-for="article in data?.contents" :key="article.id" cols="6">
-      <v-card class="article-card" :to="`/article/${article.id}`">
-        <span class="eyecatch-emoji">{{ article.emoji }}</span>
-        {{ article.title }}
-      </v-card>
+    <v-col cols="12" style="max-width: 900px">
+      <v-row>
+        <v-col
+          v-for="article in data?.contents"
+          :key="article.id"
+          cols="12"
+          sm="6"
+        >
+          <v-card class="article-card" :to="`/article/${article.id}`" ripple>
+            <span class="eyecatch-emoji">{{ article.emoji }}</span>
+            {{
+              article.title
+            }}こんにちは。さようなら。挨拶は大切ですよね。これは成功でいいですか？
+          </v-card>
+        </v-col>
+      </v-row>
     </v-col>
   </v-row>
 </template>
@@ -31,12 +44,14 @@ useSeoMeta({
   display: flex;
   align-items: center;
   font-size: larger;
+  padding: 0.7rem;
 
   .eyecatch-emoji {
-    height: 100%;
     font-size: 2.5rem;
     background-color: #4a4b4e;
     margin-right: 1rem;
+    border-radius: 7px;
+    padding: 0.2rem 0.3rem;
   }
 }
 </style>
