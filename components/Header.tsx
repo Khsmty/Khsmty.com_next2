87da7a38e96c94e74090d9b8a0b821223690751e
Khsmty <me@khsmty.com>
2023-07-06 12:12:26 +0900

    icon size

--- components/Header.tsx
+++ components/Header.tsx
@@ -64,7 +64,7 @@ export default function Nav() {
           <div className="navbar mx-auto max-w-5xl">
             <div className="flex-none sm:hidden">
               <label htmlFor="drawer" className="btn-ghost btn-square btn">
-                <FontAwesomeIcon icon={faBars} className="h-5 w-5" />
+                <FontAwesomeIcon icon={faBars} size="lg" />
               </label>
             </div>
 
@@ -84,7 +84,7 @@ export default function Nav() {
             {/* スマホ検索ボタン */}
             <div className="flex-none sm:hidden">
               <Link href="/search" className="btn-ghost btn-square btn">
-                <FontAwesomeIcon icon={faMagnifyingGlass} className="h-5 w-5" />
+                <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" />
               </Link>
             </div>
 
@@ -96,7 +96,7 @@ export default function Nav() {
                   key={link.name}
                   className="btn-ghost btn px-3"
                 >
-                  <FontAwesomeIcon icon={link.icon} className="h-4 w-4" />
+                  <FontAwesomeIcon icon={link.icon} />
                   <span className="text-base font-normal">{link.name}</span>
                 </Link>
               ))}
@@ -112,7 +112,7 @@ export default function Nav() {
           {links.map((link) => (
             <li key={link.name}>
               <Link href={link.to}>
-                <FontAwesomeIcon icon={link.icon} className="h-4 w-4" />
+                <FontAwesomeIcon icon={link.icon} />
                 <span className="text-base font-normal">{link.name}</span>
               </Link>
             </li>
