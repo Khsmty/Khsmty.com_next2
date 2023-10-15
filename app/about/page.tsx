a46b013141e46aa2d67c7f5a7eeb68588bfc205d
Khsmty <me@khsmty.com>
2023-10-15 22:57:27 +0900

    fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,3 @@
-import { Authors, allAuthors } from 'contentlayer/generated'
 import { MDXLayoutRenderer } from 'pliny/mdx-components'
 import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
@@ -7,14 +6,5 @@ import { genPageMetadata } from 'app/seo'
 export const metadata = genPageMetadata({ title: 'About' })
 
 export default function Page() {
-  const author = allAuthors.find((p) => p.slug === 'default') as Authors
-  const mainContent = coreContent(author)
-
-  return (
-    <>
-      <AuthorLayout content={mainContent}>
-        <MDXLayoutRenderer code={author.body.code} />
-      </AuthorLayout>
-    </>
-  )
+  return <p>profile</p>
 }
