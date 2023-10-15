8e61ac81c44fd49e2839a48f7b7ed798575e4584
Khsmty <me@khsmty.com>
2023-10-15 23:00:00 +0900

    fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,3 @@
-import { MDXLayoutRenderer } from 'pliny/mdx-components'
-import AuthorLayout from '@/layouts/AuthorLayout'
-import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
 
 export const metadata = genPageMetadata({ title: 'About' })
