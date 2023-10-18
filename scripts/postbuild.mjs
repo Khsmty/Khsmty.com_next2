8d53973a2263629347df1d03ed8f88162c010743
Khsmty <me@khsmty.com>
2023-10-18 17:52:41 +0900

    fix

--- scripts/postbuild.mjs
+++ scripts/postbuild.mjs
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import rss from './rss.mjs';
 
 async function postbuild() {
--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -2,49 +2,49 @@ import { writeFileSync, mkdirSync } from 'fs';
 import path from 'path';
 import { slug } from 'github-slugger';
 import { escape } from 'pliny/utils/htmlEscaper.js';
-import siteMetadata from '../data/siteMetadata.js';
 import tagData from '../app/tag-data.json' assert { type: 'json' };
 import { allArticles } from '../.contentlayer/generated/index.mjs';
 import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer.js';
 
-const generateRssItem = (config, post) => `
+function createRSSItemXml(post) {
+  return `
   <item>
-    <guid>${config.siteUrl}/article/${post.slug}</guid>
+    <guid>${process.env.NEXT_PUBLIC_BASE_URL}/article/${post.slug}</guid>
     <title>${escape(post.title)}</title>
-    <link>${config.siteUrl}/article/${post.slug}</link>
+    <link>${process.env.NEXT_PUBLIC_BASE_URL}/article/${post.slug}</link>
     ${post.summary && `<description>${escape(post.summary)}</description>`}
     <pubDate>${new Date(post.date).toUTCString()}</pubDate>
-    <author>${config.email} (${config.author})</author>
+    <author>me@khsmty.com (Khsmty)</author>
     ${post.tags && post.tags.map((t) => `<category>${t}</category>`).join('')}
   </item>
 `;
+}
 
-function createRSSXml(config, posts, page = 'feed.xml') {
+function createRSSXml(posts, page = 'feed.xml') {
   return `
   <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
-      <title>${escape(config.title)}</title>
-      <link>${config.siteUrl}/article</link>
-      <description>${escape(config.description)}</description>
-      <language>${config.language}</language>
-      <managingEditor>${config.email} (${config.author})</managingEditor>
-      <webMaster>${config.email} (${config.author})</webMaster>
+      <title>Khsmties</title>
+      <link>${process.env.NEXT_PUBLIC_BASE_URL}/article</link>
+      <description>Khsmty のブログサイト</description>
+      <language>ja-jp</language>
+      <managingEditor>me@khsmty.com (Khsmty)</managingEditor>
+      <webMaster>me@khsmty.com (Khsmty)</webMaster>
       <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
       <atom:link href="${
-        config.siteUrl
+        process.env.NEXT_PUBLIC_BASE_URL
       }/${page}" rel="self" type="application/rss+xml"/>
-      ${posts.map((post) => generateRssItem(config, post)).join('')}
+      ${posts.map((post) => createRSSItemXml(post)).join('')}
     </channel>
   </rss>
 `;
 }
 
-async function generateRSS(config, allArticles, page = 'feed.xml') {
+async function generateRSS(allArticles, page = 'feed.xml') {
   const publishPosts = allArticles.filter((post) => post.draft !== true);
-  console.log(sortPosts(publishPosts)[0].date);
 
   if (publishPosts.length > 0) {
-    const rss = createRSSXml(config, sortPosts(publishPosts));
+    const rss = createRSSXml(sortPosts(publishPosts));
     writeFileSync(`./public/${page}`, rss);
   }
 
@@ -57,7 +57,7 @@ async function generateRSS(config, allArticles, page = 'feed.xml') {
           ),
         ),
       );
-      const rss = createRSSXml(config, filteredPosts, `tag/${tag}/${page}`);
+      const rss = createRSSXml(filteredPosts, `tag/${tag}/${page}`);
       const rssPath = path.join('public', 'tag', tag);
 
       mkdirSync(rssPath, { recursive: true });
@@ -67,7 +67,7 @@ async function generateRSS(config, allArticles, page = 'feed.xml') {
 }
 
 const rss = () => {
-  generateRSS(siteMetadata, allArticles);
+  generateRSS(allArticles);
   console.log('RSS feed generated...');
 };
 export default rss;
