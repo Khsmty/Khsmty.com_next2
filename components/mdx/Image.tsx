2248667502540614858ca84feee868756d72e84b
Khsmty <me@khsmty.com>
2023-10-19 16:12:39 +0900

    remove debug

--- components/mdx/Image.tsx
+++ components/mdx/Image.tsx
@@ -4,7 +4,6 @@ import fs from 'fs';
 
 export default async function Image(props: ImageProps) {
   const { src } = props;
-  console.log('src', src);
   let dimensions;
   if ((src as string).startsWith('/')) {
     if (fs.existsSync(`${process.cwd()}/public${src}`)) {
